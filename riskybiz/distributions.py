05d26eee53189e24d054ec36e55d066288a028ec
Michael Fitzke <michaelfitzke@gmx.de>
2015-09-14 16:22:37 +0200

    density funcs and panjer prototype

--- riskybiz/distributions.py
+++ riskybiz/distributions.py
@@ -1,4 +1,24 @@
-from math import exp
+from math import exp, factorial
+import numpy as np
+from numpy import vectorize
+
+
+class poisson(object):
+  
+  def __init__(self, lam=None):
+    self.lam = lam
+
+  def _pdf(self, k):
+    """dont use this directly"""
+    exp(- self.lam) * (lam ** k)/ factorial(k)
+
+
+  def pdf(self, k):
+    if type(k) == np.ndarray or type(k) == list:
+      vpdf = vectorize(self._pdf)
+      return vpdf(k)
+    else:
+      return self._pdf(k)
 
 
 # exponential distribution
@@ -6,4 +26,7 @@ def exponential_density(x, lam=1):
   if x < 0:
     return 0
   else: 
-    return lam* exp(-lam * x)
+    return lam* np.exp(-lam * x)
+
+def ev_density(x, a = 1, b= 1, c = 1):
+  pass
